64718c686f4b8a4eeaaf3d44688fbba463ce4c29
liu_kun <liu_kun@msn.com>
2015-03-19 21:00:29 +0800

    瞎搞前端

---
+++ src/main/webapp/ecms/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('ecms.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'ecms'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
